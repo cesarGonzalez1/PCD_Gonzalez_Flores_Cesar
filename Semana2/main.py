140ded107f93b86a4f36923d837876d9e7e1be18
cesarGonzalez1 <gonzalez.flores.cesar0gmail.com>
2026-03-19 18:08:56 -0600

    semana 2 p2

--- Semana2/main.py
+++ Semana2/main.py
@@ -4,12 +4,23 @@ def main():
 
     print("ciudad,temperatura_celsius,clasificacion")
     
+    primera_linea = True
+    
     for linea in sys.stdin:
-       
+        if primera_linea:
+            primera_linea = False
+            continue
+            
         partes = linea.strip().split(',')
-        if len(partes) == 3:
-            ciudad = partes[0]
-            print(f"{ciudad},0.0,Pendiente")
+        ciudad, temp_val, unidad = partes[0], float(partes[1]), partes[2].upper()
+
+        celsius = (temp_val - 32) * 5 / 9 if unidad == 'F' else temp_val
+        
+        if celsius < 0: cat = "Congelante"
+        elif celsius <= 15: cat = "Frio"
+        else: cat = "Otro"
+        
+        print(f"{ciudad},{celsius:.1f},{cat}")
 
 if __name__ == "__main__":
     main()
