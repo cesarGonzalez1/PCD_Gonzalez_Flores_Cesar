1ed9702fe0cbf21d583d26c18ed95866898c58e6
cesarGonzalez1 <gonzalez.flores.cesar0gmail.com>
2026-03-27 13:12:32 -0600

    seccionando la info y validando si existen o no

--- Semana 3/main.py
+++ Semana 3/main.py
@@ -16,3 +16,23 @@ def main():
             continue
 
         partes = linea.split(',')
+        if len(partes) != 4:
+            continue
+
+        nombre_prod = partes[1]
+
+        try:
+            cant = int(partes[2])
+            valor = float(partes[3])
+        except:
+            continue
+
+        info = productos.get(nombre_prod)
+        if info is None:
+            productos[nombre_prod] = {
+                "unidades": cant,
+                "ingreso": cant * valor
+            }
+        else:
+            info["unidades"] = info["unidades"] + cant
+            info["ingreso"] = info["ingreso"] + (cant * valor)
