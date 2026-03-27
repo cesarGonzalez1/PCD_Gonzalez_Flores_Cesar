669847e53fe213b78e1343366ef9aa315a14306c
cesarGonzalez1 <gonzalez.flores.cesar0gmail.com>
2026-03-27 14:20:09 -0600

    main

--- Semana 3/main.py
+++ Semana 3/main.py
@@ -36,3 +36,29 @@ def main():
         else:
             info["unidades"] = info["unidades"] + cant
             info["ingreso"] = info["ingreso"] + (cant * valor)
+
+    for clave, info in productos.items():
+        total_u = info["unidades"]
+        total_ing = info["ingreso"]
+
+        info["promedio"] = (total_ing / total_u) if total_u != 0 else 0
+
+    lista_ordenada = sorted(
+        productos.items(),
+        key=lambda elemento: elemento[1]["ingreso"],
+        reverse=True
+    )
+
+    encabezado = "producto,unidades_vendidas,ingreso_total,precio_promedio"
+    print(encabezado)
+
+    for prod, info in lista_ordenada:
+        unidades = info["unidades"]
+        ingreso = info["ingreso"]
+        promedio = info["promedio"]
+
+        print(f"{prod},{unidades},{ingreso:.2f},{promedio:.2f}")
+
+
+if __name__ == "__main__":
+    main()
